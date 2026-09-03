acd7799309c98a78176247547be00cdebb31c213
Auralith Rebuild <rebuild@auralith.dev>
2026-09-03 07:18:37 +0000

    build: rc.23 installer + Android Remote debug APK workflow

--- apps/auralith-remote-android/app/build.gradle.kts
+++ apps/auralith-remote-android/app/build.gradle.kts
@@ -12,14 +12,9 @@ android {
     versionCode = 1
     versionName = "1.0.0-remote.1"
   }
-  buildFeatures { compose = true }
-  composeOptions { kotlinCompilerExtensionVersion = "1.5.15" }
   buildTypes { release { isMinifyEnabled = false } }
 }
 dependencies {
   implementation("androidx.core:core-ktx:1.15.0")
-  implementation("androidx.activity:activity-compose:1.9.3")
-  implementation("androidx.compose.ui:ui:1.7.6")
-  implementation("androidx.compose.material3:material3:1.3.1")
   implementation("com.squareup.okhttp3:okhttp:4.12.0")
 }
