6762b528068ad5e112a477fc8ca53ba0bab26562
dragonking587-ai <dragonking587@gmail.com>
2026-09-08 11:43:22 -0500

    feat(unreal): add bridge plugin build rules

---
+++ unreal_bridge/plugin/AuralithBridge/Source/AuralithBridge/AuralithBridge.Build.cs
@@ -0,0 +1,21 @@
+using UnrealBuildTool;
+
+public class AuralithBridge : ModuleRules
+{
+    public AuralithBridge(ReadOnlyTargetRules Target) : base(Target)
+    {
+        PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+
+        PublicDependencyModuleNames.AddRange(new string[]
+        {
+            "Core",
+            "CoreUObject",
+            "Engine"
+        });
+
+        PrivateDependencyModuleNames.AddRange(new string[]
+        {
+            "UnrealEd"
+        });
+    }
+}
