d704bb624739b9594764a938875e3a6d1546a2f9
dragonking587-ai <dragonking587@gmail.com>
2026-09-08 11:43:46 -0500

    feat(unreal): add bridge plugin module implementation

---
+++ unreal_bridge/plugin/AuralithBridge/Source/AuralithBridge/Private/AuralithBridgeModule.cpp
@@ -0,0 +1,12 @@
+#include "AuralithBridgeModule.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_MODULE(FAuralithBridgeModule, AuralithBridge)
+
+void FAuralithBridgeModule::StartupModule()
+{
+}
+
+void FAuralithBridgeModule::ShutdownModule()
+{
+}
